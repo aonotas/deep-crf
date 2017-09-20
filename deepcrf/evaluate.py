622b984c793d8bcb5048f0722b8820b1df596f8b
aonotas <nanigashi03@gmail.com>
2017-09-20 15:48:33 +0900

    fix eval code

--- deepcrf/evaluate.py
+++ deepcrf/evaluate.py
@@ -1,4 +1,4 @@
-
+import re
 import util
 import util_talbes
 
@@ -29,6 +29,14 @@ def uniq_tagset(alltags_list, tag_names=[]):
     return tag_names
 
 
+def replace_S_E_tags(tag_lists):
+    def rep_func(tag):
+        tag = re.sub(r'^S-', "B-", tag)
+        tag = re.sub(r'^E-', "I-", tag)
+        return tag
+    return [[rep_func(tag) for tag in tags] for tags in tag_lists]
+
+
 def run(gold_file, predicted_file, **args):
     gold_tags = load_file(gold_file)
     predicted_tags = load_file(predicted_file)
@@ -37,6 +45,8 @@ def run(gold_file, predicted_file, **args):
     tag_names = []
     tag_names = uniq_tagset(gold_tags, tag_names)
     tag_names = uniq_tagset(predicted_tags, tag_names)
+    gold_tags = replace_S_E_tags(gold_tags)
+    predicted_tags = replace_S_E_tags(predicted_tags)
     gold_predict_pairs = [gold_tags, predicted_tags]
     result, phrase_info = util.conll_eval(gold_predict_pairs, flag=False, tag_class=tag_names)
 
